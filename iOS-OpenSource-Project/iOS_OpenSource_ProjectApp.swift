8330137b2619f353b317d9cdb271e1afebb07b8a
dishamane23 <manedisha67@gmail.com>
2024-12-23 20:50:49 +0530

    Initial Commit

---
+++ iOS-OpenSource-Project/iOS_OpenSource_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_OpenSource_ProjectApp.swift
+//  iOS-OpenSource-Project
+//
+//  Created by anket kohak on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_OpenSource_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
